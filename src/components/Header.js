0d74e73f19466ec67b70e9752d21ac63df23d802
Maksquare <fikrumak@email.com>
2023-03-01 20:25:50 +0300

    addded phone and email to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React from "react";
 // import logo
-import Logo from '../assets/logo.png'
+import Logo from "../assets/logo.png";
 
 // import  icons
-import phone from '../assets/icons/call.png'
-import email from '../assets/icons/email.png'
+import phone from "../assets/icons/call.png";
+import email from "../assets/icons/email.png";
 
 const Header = () => {
-  return <div className=' py-8'>
-    <div className="container mx-auto ">
-      <div className='flex justify-between '>
-      {/* logo */}
-        <a href="#">
-          <img src={Logo} alt=""  className='w-[250px] '/>
-        </a>
-        {/* button */}
-        <button className='work-btn btn btn-sm relative'>Work with us
-        
-        <div className='info absolute p-10 inset-0 gradient w-[200px]  rounded-md  flex flex-col items-start justify-center gap-y-2 '>
-           
-          <img  className='w-6' src={email} alt="" />
-          <img  className='w-6' src={phone} alt="" />
+  return (
+    <div className=" py-8">
+      <div className="container mx-auto ">
+        <div className="flex justify-between ">
+          {/* logo */}
+          <a href="#">
+            <img src={Logo} alt="" className="w-[250px] " />
+          </a>
+          {/* button */}
+          <button className="work-btn btn btn-sm relative">
+            Work with us
+            <div className="info absolute p-2 inset-0 gradient w-[210px] h-[100px] rounded-md  flex flex-col items-start justify-center gap-y-5 ">
+              <div className="flex gap-x-4">
+                <img className="w-6" src={email} alt="" />
+                <span> somickk@gmail.com</span>
+              </div>
+              <div className="flex gap-x-4">
+                <img className="w-6" src={phone} alt="" />
+                <span> +251-911261954</span>
+              </div>
+            </div>
+          </button>
         </div>
-        </button>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default Header;
